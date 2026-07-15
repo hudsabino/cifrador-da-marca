28be3cfd13915dd2281e9a242b2f23aa619ec7c7
Hudson Sabino <hudson.mu@gmail.com>
2026-07-15 09:48:57 -0300

    Atualiza para Marca do Marcador

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "cifrador-da-marca-v12";
+const ARQUIVOS = [
+  "./", "./index.html", "./manifest.webmanifest",
+  "./favicon.svg", "./icone-192.png", "./icone-512.png",
+  "./icone-maskable-512.png", "./apple-touch-icon.png", "./emblema-glpmmmce.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
